9e7f992ba1f16b675ec06028c5f7a6b85e07cb5d
Conor O'Brien <conorobrien4god@gmail.com>
2016-03-17 20:57:58 -0400

    Create Jolf.js

---
+++ Jolf.js
@@ -0,0 +1,18 @@
+function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
+
+loadScript("https://rawgit.com/ConorOBrien-Foxx/Jolf/master/src/jolf.js");
